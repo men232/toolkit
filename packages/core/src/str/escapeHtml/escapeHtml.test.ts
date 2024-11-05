e6f3e92aa201d41f2b23da7263aa77b3d599ede9
Andrew L <andrew.io.dev@gmail.com>
2024-11-05 20:23:23 +0100

    perf(escapeHtml) o(n) instead of o(n * r * 5)

--- packages/core/src/str/escapeHtml/escapeHtml.test.ts
+++ packages/core/src/str/escapeHtml/escapeHtml.test.ts
@@ -1,8 +1,24 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { escapeHtml } from './escapeHtml';
 
-describe('convertToUnit', () => {
-  test('defaults', () => {
-    expect(escapeHtml('<b>Hello</b>')).toBe('&lt;b&gt;Hello&lt;/b&gt;');
+describe('escapeHtml', () => {
+  let escaped = '&amp;&lt;&gt;&quot;&#39;/';
+  let unescaped = '&<>"\'/';
+
+  escaped += escaped;
+  unescaped += unescaped;
+
+  it('should escape values', () => {
+    expect(escapeHtml(unescaped)).toBe(escaped);
+  });
+
+  it('should handle strings with nothing to escape', () => {
+    expect(escapeHtml('abc')).toBe('abc');
+  });
+
+  ['`', '/'].forEach(chr => {
+    it(`should not escape the "${chr}" character`, () => {
+      expect(escapeHtml(chr)).toBe(chr);
+    });
   });
 });
--- packages/core/src/str/escapeHtml/escapeHtml.ts
+++ packages/core/src/str/escapeHtml/escapeHtml.ts
@@ -1,3 +1,11 @@
+const htmlEscapes: Record<number, string> = Object.freeze({
+  38: '&amp;', // &
+  60: '&lt;', // <
+  62: '&gt;', // >
+  34: '&quot;', // "
+  39: '&#39;', // '
+});
+
 /**
  * Sanitize a string to escape html syntax
  *
@@ -11,10 +19,14 @@ export function escapeHtml(unsafe: string) {
     return '';
   }
 
-  return unsafe
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#039;');
+  let result = '';
+  let strLen = unsafe.length;
+  let char: number;
+
+  for (let idx = 0; idx < strLen; idx++) {
+    char = unsafe.charCodeAt(idx);
+    result += htmlEscapes[char] || String.fromCharCode(char);
+  }
+
+  return result;
 }
