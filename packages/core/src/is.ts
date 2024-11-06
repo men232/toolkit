55cbc2cd2b6e2825589259333dbb9db6e84da58d
Andrew L <andrew.io.dev@gmail.com>
2024-11-06 21:18:49 +0100

    feat(is) more predicates

--- packages/core/src/is.ts
+++ packages/core/src/is.ts
@@ -3,82 +3,88 @@ import type { Primitive } from './types';
 /**
  * Determines if the window object is available in the global scope
  *
- * @group Is
+ * @group Predicates
  */
 export const isClient = typeof (globalThis as any)?.window !== 'undefined';
 
 /**
  * Returns `true` when value is not `undefined`
- * @group Is
+ * @group Predicates
  */
 export const isDef = <T = any>(val?: T): val is T => typeof val !== 'undefined';
 
 const toString = Object.prototype.toString;
 
 /**
- * Returns `true` when value is `null` or `undefined`
- * @group Is
+ * Checks if the given value is a `null` or `undefined`
+ * @group Predicates
  */
 export function isNullOrUndefined(value: unknown): value is undefined | null {
   return value === null || value === undefined;
 }
 
 /**
- * Returns `true` when value is `BigInt`
- * @group Is
+ * Checks if the given value is a `bigint`
+ * @group Predicates
  */
 export const isBigInt = (val: any): val is bigint => typeof val === 'bigint';
 
 /**
- * Returns `true` when value is `Boolean`
- * @group Is
+ * Checks if the given value is a `boolean`
+ * @group Predicates
  */
 export const isBoolean = (val: any): val is boolean => typeof val === 'boolean';
 
 /**
- * Returns `true` when value is `Function`
- * @group Is
+ * Checks if the given value is a `function`
+ * @group Predicates
  */
 export const isFunction = <T extends Function>(val: any): val is T =>
   typeof val === 'function';
 
 /**
- * Returns `true` when value is `Number` and not `NaN`
- * @group Is
+ * Checks if the given value is a `number`
+ *
+ * @example
+ * console.log(isNumber(123)); // true
+ * console.log(isNumber('abc')); // false
+ * console.log(isNumber(NaN)); // false
+ *
+ * @group Predicates
  */
 export const isNumber = (val: any): val is number =>
   typeof val === 'number' && !isNaN(val);
 
 /**
- * Returns `true` when value is `String`
- * @group Is
+ * Checks if the given value is a `string`
+ * @group Predicates
  */
 export const isString = (val: unknown): val is string =>
   typeof val === 'string';
 
 /**
- * Returns `true` when value is plain `Object`
- * @group Is
+ * Checks if the given value is a plain `object`
+ * @group Predicates
  */
 export const isObject = (val: any): val is object =>
   toString.call(val) === '[object Object]';
 
 /**
- * Returns `true` when value is valid `Date`
- * @group Is
+ * Checks if the given value is valid `Date`
+ * @group Predicates
  */
 export const isDate = (val: any): val is Date =>
   val instanceof Date && !isNaN(val as any);
 
 /**
  * Function that does nothing
- * @group Others
+ * @group Utility Functions
  */
 export const noop = () => {};
 
 /**
- * Returns true if values is `Error` or instance of `Error`
- * @group Is
+ * Checks if the given value is a `Error`
+ * @group Predicates
  */
 export const isError = (val: any): val is Error =>
   val instanceof Error ||
@@ -86,10 +92,38 @@ export const isError = (val: any): val is Error =>
   (isObject(val) && isString(val.message) && isString(val.stack));
 
 /**
- * Returns true if values is `symbol`
- * @group Is
+ * Checks if the given value is a `symbol`
+ * @group Predicates
  */
 export const isSymbol = (val: any): val is Symbol => typeof val == 'symbol';
+
+/**
+ * Checks if the given value is a `Set`.
+ * @group Predicates
+ */
+export const isSet = <T = any>(val: any): val is Set<T> => val instanceof Set;
+
+/**
+ * Checks if the given value is a `WeekSet`.
+ * @group Predicates
+ */
+export const isWeakSet = <T extends WeakKey = any>(
+  val: any,
+): val is WeakSet<T> => val instanceof WeakSet;
+
+/**
+ * Checks if the given value is a `Map`.
+ * @group Predicates
+ */
+export const isMap = <K = any, V = any>(val: any): val is Map<K, V> =>
+  val instanceof Map;
+
+/**
+ * Checks if the given value is a `WeakMap`.
+ * @group Predicates
+ */
+export const isWeakMap = <V = any>(val: any): val is WeakMap<WeakKey, V> =>
+  val instanceof WeakMap;
 
 /**
  * Checks if two values are equal, including support for `Date`, `RegExp`, and deep object comparison.
@@ -105,7 +139,7 @@ export const isSymbol = (val: any): val is Symbol => typeof val == 'symbol';
  * isEqual(new Date('2020-01-01'), new Date('2020-01-01')); // true
  * isEqual([1, 2, 3], [1, 2, 3]); // true
  *
- * @group Is
+ * @group Predicates
  */
 export function isEqual(a: unknown, b: unknown): boolean {
   if (Object.is(a, b)) {
@@ -155,9 +189,25 @@ export function isEqual(a: unknown, b: unknown): boolean {
 }
 
 /**
- * Returns true if values is empty. Including support for `Array`, `Object`, `String` `Map`, `Set`.
+ * Checks if a given value is empty.
  *
- * @group Is
+ * Support for `Array`, `Object`, `string` `Map`, `Set`.
+ *
+ * @example
+ * isEmpty(); // true
+ * isEmpty(null); // true
+ * isEmpty(''); // true
+ * isEmpty([]); // true
+ * isEmpty({}); // true
+ * isEmpty(new Map()); // true
+ * isEmpty(new Set()); // true
+ * isEmpty('hello'); // false
+ * isEmpty([1, 2, 3]); // false
+ * isEmpty({ a: 1 }); // false
+ * isEmpty(new Map([['key', 'value']])); // false
+ * isEmpty(new Set([1, 2, 3])); // false
+ *
+ * @group Predicates
  */
 export const isEmpty = (obj: any): boolean => {
   if (obj === null || obj === undefined) return true;
@@ -196,8 +246,8 @@ export const isEmpty = (obj: any): boolean => {
 };
 
 /**
- * Returns `true` when value is `Promise`
- * @group Is
+ * Checks if the given value is a `Promise`
+ * @group Predicates
  */
 export function isPromise<T = void>(value: any): value is Promise<T> {
   return (
@@ -215,8 +265,10 @@ const primitiveTypeofSet = Object.freeze(
 );
 
 /**
- * Returns `true` when value is a primitive. `String`, `Number`, `Boolean`, `BigInt`, `Symbol`, `undefined`
- * @group Is
+ * Checks whether a value is a JavaScript primitive.
+ *
+ * JavaScript primitives include null, undefined, strings, numbers, booleans, symbols, and bigints.
+ * @group Predicates
  */
 export const isPrimitive = (value: unknown): value is Primitive => {
   return value === null || primitiveTypeofSet.has(typeof value);
