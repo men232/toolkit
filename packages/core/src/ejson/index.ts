716bdc6dd8fcb06ef4a7e841096af36c70588e0f
Andrew L <andrew.io.dev@gmail.com>
2025-05-11 19:33:22 +0200

    fix(ejson) export ejson instance type

--- packages/core/src/ejson/index.ts
+++ packages/core/src/ejson/index.ts
@@ -2,3 +2,5 @@ export * from './createEJSON';
 export * from './createEJSONStream';
 export * from './EJSONStream';
 export { instance as EJSON } from './instance';
+
+export { EJSON as EJSONInstance, type EJSONType } from './EJSON.js';
