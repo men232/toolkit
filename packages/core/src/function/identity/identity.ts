07440743624dbcccd1d68efa89a55c1132321f38
Andrew L <andrew.io.dev@gmail.com>
2026-07-08 16:19:16 +0200

    feat(core): identity

---
+++ packages/core/src/function/identity/identity.ts
@@ -0,0 +1,24 @@
+/**
+ * Returns the input value unchanged.
+ *
+ * @template T - The type of the input value.
+ * @param x - The value to be returned.
+ * @returns The input value.
+ *
+ * @example
+ * // Returns 5
+ * identity(5);
+ *
+ * @example
+ * // Returns 'hello'
+ * identity('hello');
+ *
+ * @example
+ * // Returns { key: 'value' }
+ * identity({ key: 'value' });
+ *
+ * @group Utility Functions
+ */
+export function identity<T>(x: T): T {
+  return x;
+}
